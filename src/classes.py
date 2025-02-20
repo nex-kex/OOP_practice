9abd4d074665265eb9dd0e0324bd524f2e6377ea
Mark Volkov <markasch67@gmail.com>
2025-02-20 14:47:20 +0300

    Extra tasks

--- src/classes.py
+++ src/classes.py
@@ -1,11 +1,15 @@
 class Product:
     """Класс для представления продуктов."""
 
+    __all_products = []
+
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
+
+        Product.__all_products.append(self)
 
 
     @property
@@ -17,12 +21,26 @@ class Product:
     def price(self, new_price):
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
-        else:
-            self.__price = new_price
+        elif new_price < self.__price:
+            answer = input(f"Новая цена товара ({new_price}) ниже предыдущей ({self.__price}). Вы уверены, что вы хотите снизить цену? (y/n) ")
+            if answer == "y":
+                self.__price = new_price
 
 
     @classmethod
     def new_product(cls, product_dict):
+
+        name = product_dict["name"]
+        for product in Product.__all_products:
+
+            if product.name == name:
+                product.quantity += product_dict["quantity"]
+
+                if product.price != product_dict["price"]:
+                    product.__price = max(product.price, product_dict["price"])
+
+                return product
+
         return Product(**product_dict)
 
 
@@ -48,5 +66,5 @@ class Category:
     @property
     def products(self):
         for product in self.__products:
-            print(f"{product.name}, {product._Product__price} руб. Остаток: {product.quantity} шт.")
+            print(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
         return self.__products
