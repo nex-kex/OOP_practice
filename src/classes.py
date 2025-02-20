5cd513c3b145694e81e4ffe096eb02fa2bb0b282
Mark Volkov <markasch67@gmail.com>
2025-02-20 15:37:19 +0300

    Added tests

--- src/classes.py
+++ src/classes.py
@@ -1,7 +1,10 @@
+from typing import Any
+
+
 class Product:
     """Класс для представления продуктов."""
 
-    __all_products = []
+    __all_products: list = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
@@ -11,24 +14,27 @@ class Product:
 
         Product.__all_products.append(self)
 
-
     @property
-    def price(self):
+    def price(self) -> float:
         return self.__price
 
-
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price: float) -> None:
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         elif new_price < self.__price:
-            answer = input(f"Новая цена товара ({new_price}) ниже предыдущей ({self.__price}). Вы уверены, что вы хотите снизить цену? (y/n) ")
+            print(
+                f"Новая цена товара ({new_price}) ниже предыдущей ({self.__price}). \
+Вы уверены, что вы хотите снизить цену? (y/n)"
+            )
+            answer = input()
             if answer == "y":
                 self.__price = new_price
-
+        elif new_price >= self.__price:
+            self.__price = new_price
 
     @classmethod
-    def new_product(cls, product_dict):
+    def new_product(cls, product_dict: dict) -> Any:
 
         name = product_dict["name"]
         for product in Product.__all_products:
@@ -58,13 +64,12 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.__products)
 
-
-    def add_product(self, product):
+    def add_product(self, product: Product) -> None:
         self.__products.append(product)
         Category.product_count += 1
 
     @property
-    def products(self):
+    def products(self) -> list:
         for product in self.__products:
             print(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
         return self.__products
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,4 +1,6 @@
-from src.classes import Category
+from unittest.mock import patch
+
+from src.classes import Category, Product
 
 
 def test_product_init(product_phone):
@@ -6,6 +8,61 @@ def test_product_init(product_phone):
     assert product_phone.description == "256GB, Серый цвет, 200MP камера"
     assert product_phone.price == 180000.0
     assert product_phone.quantity == 5
+
+
+def test_price_setter_zero(product_phone, capsys):
+    product_phone.price = 0
+    captured = capsys.readouterr()
+    assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
+
+
+def test_price_setter_new_lower_price_changed(product_phone, capsys):
+    with patch("builtins.input", return_value="y"):
+        product_phone.price = 100
+        captured = capsys.readouterr()
+        assert (
+            captured.out
+            == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
+        )
+        assert product_phone.price == 100
+
+
+def test_price_setter_new_lower_price_not_changed(product_phone, capsys):
+    with patch("builtins.input", return_value="n"):
+        product_phone.price = 100
+        captured = capsys.readouterr()
+        assert (
+            captured.out
+            == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
+        )
+        assert product_phone.price == 180000.0
+
+
+def test_price_setter_new_higher_price(product_phone):
+    product_phone.price = 1000000.00
+    assert product_phone.price == 1000000.00
+
+
+def test_new_product():
+    new_product = Product.new_product(
+        {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
+    )
+    assert new_product.name == '55" QLED 4K'
+    assert new_product.description == "Фоновая подсветка"
+    assert new_product.price == 123000.0
+    assert new_product.quantity == 7
+
+    assert len(new_product._Product__all_products) == 6
+
+
+def test_new_new_product(product_dict):
+    new_product = Product.new_product(product_dict)
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+    assert new_product.description == "256GB, Серый цвет, 200MP камера"
+    assert new_product.price == 280000.0
+    assert new_product.quantity == 8
+
+    assert len(new_product._Product__all_products) == 6
 
 
 def test_category_init(category_phones, category_laptops):
@@ -20,3 +77,9 @@ def test_category_init(category_phones, category_laptops):
     assert category_phones.product_count == 7
     assert category_laptops.product_count == 7
     assert Category.product_count == 7
+
+
+def test_add_product(category_phones, product_phone):
+    assert Category.product_count == 10
+    category_phones.add_product(product_phone)
+    assert Category.product_count == 11
