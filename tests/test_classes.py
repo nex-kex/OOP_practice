e3787ae1be58fec33d2a71d67609d39f6788e40b
Mark Volkov <markasch67@gmail.com>
2025-03-02 00:30:05 +0300

    Task 4

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,11 @@
 from unittest.mock import patch
 
-from src.classes import Category, CategoryIteration, Product
+import pytest
+
+from src.classes import Category, CategoryIteration, Product, Smartphone, LawnGrass
+
+
+# Product tests
 
 
 def test_product_init(product_phone):
@@ -21,8 +26,8 @@ def test_price_setter_new_lower_price_changed(product_phone, capsys):
         product_phone.price = 100
         captured = capsys.readouterr()
         assert (
-            captured.out
-            == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
+                captured.out
+                == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
         )
         assert product_phone.price == 100
 
@@ -32,8 +37,8 @@ def test_price_setter_new_lower_price_not_changed(product_phone, capsys):
         product_phone.price = 100
         captured = capsys.readouterr()
         assert (
-            captured.out
-            == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
+                captured.out
+                == "Новая цена товара (100) ниже предыдущей (180000.0). Вы уверены, что вы хотите снизить цену? (y/n)\n"
         )
         assert product_phone.price == 180000.0
 
@@ -66,6 +71,70 @@ def test_new_new_product(product_phone, product_dict):
     assert len(new_product.all_products) == 2
 
 
+def test_product_add():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    assert product1 + product2 == 5 * 180000.0 + 8 * 210000.0
+
+    with pytest.raises(TypeError):
+        product1 + 100
+
+
+# Smartphone tests
+
+
+def test_smartphone_init(product_phone):
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                             "S23 Ultra", 256, "Серый")
+    assert smartphone1.name == "Samsung Galaxy S23 Ultra"
+    assert smartphone1.description == "256GB, Серый цвет, 200MP камера"
+    assert smartphone1.price == 180000.0
+    assert smartphone1.quantity == 5
+    assert smartphone1.efficiency == 95.5
+    assert smartphone1.model == "S23 Ultra"
+    assert smartphone1.memory == 256
+    assert smartphone1.color == "Серый"
+
+
+def test_smartphone_add():
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                             "S23 Ultra", 256, "Серый")
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+    assert smartphone1 + smartphone2 == 5 * 180000.0 + 8 * 210000.0
+
+    with pytest.raises(TypeError):
+        smartphone1 + 100
+
+
+# LawnGrass tests
+
+
+def test_lawngrass_init(product_phone):
+    lawngrass1 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+    assert lawngrass1.name == "Газонная трава 2"
+    assert lawngrass1.description == "Выносливая трава"
+    assert lawngrass1.price == 450.0
+    assert lawngrass1.quantity == 15
+    assert lawngrass1.country == "США"
+    assert lawngrass1.germination_period == "5 дней"
+    assert lawngrass1.color == "Темно-зеленый"
+
+
+def test_lawngrass_add():
+    lawngrass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    lawngrass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
+    assert lawngrass1 + lawngrass2 == 20 * 500.0 + 15 * 450.0
+
+    with pytest.raises(TypeError):
+        lawngrass1 + 100
+
+
+# Category tests
+
+
 def test_category_init(category_phones, category_laptops, product_phone):
     assert category_phones.name == "Смартфоны"
     assert category_phones.description == "Средство коммуникации"
@@ -86,12 +155,8 @@ def test_category_init(category_phones, category_laptops, product_phone):
     category_phones.add_product(product_phone)
     assert Category.product_count == 8
 
-
-def test_product_add():
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-
-    assert product1 + product2 == 5 * 180000.0 + 8 * 210000.0
+    with pytest.raises(TypeError):
+        category_phones.add_product(100)
 
 
 def test_category_str():
